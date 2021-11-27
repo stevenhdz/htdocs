eafea9aa1d14f2bcfcf68ce18c334dac0a56c9bc
sltechnology <stevenhernandezj@gmail.com>
2021-11-27 12:04:07 -0500

    sanctum auth api

--- cursoLaravel/RestApi_SanctumAuthentication/api/routes/api.php
+++ cursoLaravel/RestApi_SanctumAuthentication/api/routes/api.php
@@ -1,12 +1,17 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/register', [AuthController::class,'register']);
+Route::post('/login', [AuthController::class,'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function(){
+    Route::resource('products', ProductController::class);
+    Route::get('products/search/{name}', [ProductController::class,'search']);
+    Route::post('/logout', [AuthController::class,'logout']);
+    Route::any('/{any}', function(){ return 'url no validate'; })->where('any', '.*');
 });
-Route::resource('products', ProductController::class);
-Route::get('products/search/{name}', [ProductController::class,'search']);
-Route::any('/{any}', function(){ return 'The url no exists'; })->where('any', '.*');
+
