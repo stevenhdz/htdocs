5cf8569b6ae5767c3f7d4c87c5cd6dc6a2c9bf33
sltechnology <stevenhernandezj@gmail.com>
2021-12-16 16:45:08 -0500

    cors

---
+++ cursoLaravel/RestApi_SanctumAuthentication/api/app/Http/Middleware/Cors.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if ($request->isMethod('OPTIONS')){
+            $response = Response::make();
+        } else {
+            $response = $next($request);
+        }
+        return $response
+      ->header("Access-Control-Allow-Origin", "*")
+      ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+      ->header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application");
+    }
+}
--- cursoLaravel/RestApi_SanctumAuthentication/api/routes/api.php
+++ cursoLaravel/RestApi_SanctumAuthentication/api/routes/api.php
@@ -9,9 +9,11 @@ Route::post('/register', [AuthController::class,'register']);
 Route::post('/login', [AuthController::class,'login']);
 
 Route::group(['middleware' => ['auth:sanctum']], function(){
-    Route::resource('products', ProductController::class);
-    Route::get('products/search/{name}', [ProductController::class,'search']);
-    Route::post('/logout', [AuthController::class,'logout']);
-    Route::any('/{any}', function(){ return 'url no validate'; })->where('any', '.*');
+    Route::group(['middleware' => ['cors']], function () {
+        Route::resource('products', ProductController::class);
+        Route::get('products/search/{name}', [ProductController::class,'search']);
+        Route::post('/logout', [AuthController::class,'logout']);
+        Route::any('/{any}', function(){ return 'url no validate'; })->where('any', '.*');
+    });
 });
 
