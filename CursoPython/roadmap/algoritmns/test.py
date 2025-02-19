e8743a5cead295ea085876329024c40fd02bcda5
stevenhdz <stevenhernandezj@gmail.com>
2025-02-18 21:53:20 -0500

    fixed

--- CursoPython/roadmap/algoritmns/test.py
+++ CursoPython/roadmap/algoritmns/test.py
@@ -4,9 +4,9 @@ class test:
 
     def order_by_most_recent(self):
         sort = sorted(self.data, key=lambda item: item['createAt'], reverse=True)
-        print(sort[0])
         return sort[0]
     
 
 order = test([{"createAt": "2025-02-19T14:30:00Z"}, {"createAt": "2025-02-16T14:30:00Z"}, {"createAt": "2025-03-17T14:30:00Z"}])
-order.order_by_most_recent()
+result = order.order_by_most_recent()
+print(result)
